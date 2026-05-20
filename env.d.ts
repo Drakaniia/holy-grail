90370e967f1af81dff46458967713384cbfc8a1e
Drakaniia <floresaybaez574@gmail.com>
2026-05-20 17:42:53 +0800

    chore: add supabase-js dep; gitignore .env files; declare env var types

--- env.d.ts
+++ env.d.ts
@@ -1,5 +1,15 @@
 /// <reference types="vite/client" />
 
+interface ImportMetaEnv {
+  readonly VITE_SUPABASE_ANON_KEY?: string
+  readonly VITE_SUPABASE_PUBLISHABLE_KEY?: string
+  readonly VITE_SUPABASE_URL?: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
+
 declare module '*.vue' {
   import type { DefineComponent } from 'vue'
   const component: DefineComponent<{}, {}, any>
